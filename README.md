b22cede1d662aa65fe66487a66db9ac98831d66d
TechSunny <sunnymotivation2@gmail.com>
2024-12-04 13:58:24 +0530

    initialised project

---
+++ README.md
@@ -0,0 +1,1 @@
+# task mangement: to do project
---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+//  component: function returning jsx (html like code)
+
+import React from 'react'
+
+function App(){
+  return (
+    <h1> Schedule & Manage ur Tasks </h1>
+  )
+}
+
+
+
+
+export default App
